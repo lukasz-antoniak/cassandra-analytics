d2805bd57f32fe0e344657756af2f221aa37e4b2
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2025-11-14 15:56:17 +0100

    GitHub Actions

--- cassandra-analytics-integration-framework/src/main/java/org/apache/cassandra/testing/TestTokenSupplier.java
+++ cassandra-analytics-integration-framework/src/main/java/org/apache/cassandra/testing/TestTokenSupplier.java
@@ -21,7 +21,6 @@ package org.apache.cassandra.testing;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import org.apache.cassandra.distributed.api.TokenSupplier;
