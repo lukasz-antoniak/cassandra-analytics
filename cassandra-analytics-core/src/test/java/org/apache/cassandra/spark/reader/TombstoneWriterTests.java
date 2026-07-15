5f9d76503c64e92e53dc5d556fe2e8bacb0fbf46
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2026-07-15 11:35:23 +0200

    Code cleanup

--- cassandra-analytics-core/src/test/java/org/apache/cassandra/spark/reader/TombstoneWriterTests.java
+++ cassandra-analytics-core/src/test/java/org/apache/cassandra/spark/reader/TombstoneWriterTests.java
@@ -20,11 +20,9 @@
 package org.apache.cassandra.spark.reader;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
